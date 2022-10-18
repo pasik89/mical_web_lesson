07f57158fd32f3aab3845423be94787d233c6fd1
AGATA_IMIOLEK <imiolekagata@yahoo.com>
2022-10-18 17:15:31 +0200

    creted home page of Mical - car services

---
+++ js/main.js
@@ -0,0 +1,21 @@
+class Header extends HTMLElement {
+  constructor() {
+    super();
+  }
+  connectedCallback() {
+    this.innerHTML = `
+      <nav>
+      <a><img src="img/logo.png" alt="logo"></a>
+          <a>Home </a>
+          <a>About </a>
+          <a>Furnitures </a>
+          <a>Testimonial </a>
+          <a>Contact Us</a>
+          <a><i class="fa-regular fa-user"></i>Login/Register</a>
+          <a><i class="fa-solid fa-magnifying-glass"></i></a>
+      </nav>
+      `;
+  }
+}
+
+customElements.define('main-header', Header);
